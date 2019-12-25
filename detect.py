ac71c820013c18091eee1ed46ee88851f37e2484
galcohen <gal.cohen@autodesk.com>
2019-12-25 09:26:04 +0200

    reformat str

--- detect.py
+++ detect.py
@@ -1,11 +1,14 @@
+import math
+
 import boto3 as boto3
 import cv2
 import numpy as np
 
 
 
-image_name = 'tmp.jpg'
-brightness = 0.9
+image_name = 'cr.jpg'
+# brightness = 0.9
+brightness = 3
 
 
 def change_brightness(img, alpha, beta):
@@ -13,7 +16,7 @@ def change_brightness(img, alpha, beta):
 
 
 def take_picture(img_name):
-    cam = cv2.VideoCapture(1)
+    cam = cv2.VideoCapture(0)
     ret, frame = cam.read()
     frame = change_brightness(frame, brightness, 0)
 
@@ -21,17 +24,18 @@ def take_picture(img_name):
     print("{} written!".format(img_name))
     cam.release()
 
-font_emotions = cv2.FONT_ITALIC
-font_emotions_scale = 0.5
+font_emotions = cv2.FONT_HERSHEY_SCRIPT_COMPLEX
+font_emotions_scale = 0.3
 font_emotions_color = (0, 128, 255)
-lineType = 2
+lineType = 1
 
-total_calm = 0
-total_confused = 0
-total_surprised = 0
 
 while True:
-    # take_picture(image_name)
+    total_calm = 0
+    total_confused = 0
+    total_happy = 0
+
+    take_picture(image_name)
     with open(image_name, 'rb') as f: # open the file in read binary mode
         data = f.read()
 
@@ -63,8 +67,8 @@ while True:
             emotions_dict[type] = item['Confidence']
 
         calm = str(int(emotions_dict.get('CALM', 0)))
-        surprised = str(int(emotions_dict.get('SURPRISED', 0)))
         confused = str(int(emotions_dict.get('CONFUSED', 0)))
+        happy = str(int(emotions_dict.get('HAPPY', 0)))
 
         im_height, im_width, channels = img.shape
 
@@ -78,29 +82,29 @@ while True:
             color = (250, 206, 135)
         else:
             female +=1
-            color = (147, 112, 219)
-        img = cv2.rectangle(img, (face_left_position, face_top_position), (face_left_position+face_width, face_height+face_top_position), color, 5)
+            color = (51, 51, 255)
+        img = cv2.rectangle(img, (face_left_position, face_top_position), (face_left_position+face_width, face_height+face_top_position), color, 2)
 
         total_calm += int(calm)
         total_confused += int(confused)
-        total_surprised += int(surprised)
+        total_happy += int(happy)
 
-        calm = f'calm {calm}%'
-        surprised = f'surprised {surprised}%'
-        confused = f'confused {confused}%'
-        bottomLeftCornerOfText = (face_left_position+face_width+10, face_top_position + 40)
-        cv2.putText(img, calm, bottomLeftCornerOfText, font_emotions, font_emotions_scale, font_emotions_color, lineType)
-        bottomLeftCornerOfText = (face_left_position+face_width+10, face_top_position + 25)
-        cv2.putText(img, surprised, bottomLeftCornerOfText, font_emotions, font_emotions_scale, font_emotions_color, lineType)
-        bottomLeftCornerOfText = (face_left_position+face_width+10, face_top_position + 10)
-        cv2.putText(img, confused, bottomLeftCornerOfText, font_emotions, font_emotions_scale, font_emotions_color, lineType)
+        # calm = f'{calm}%'
+        # surprised = f'{surprised}%'
+        # confused = f'{confused}%'
+        # bottomLeftCornerOfText = (face_left_position+face_width+10, face_top_position + 40)
+        # cv2.putText(img, confused, bottomLeftCornerOfText, font_emotions, font_emotions_scale, font_emotions_color, lineType)
+        # bottomLeftCornerOfText = (face_left_position+face_width+10, face_top_position + 30)
+        # cv2.putText(img, surprised, bottomLeftCornerOfText, font_emotions, font_emotions_scale, font_emotions_color, lineType)
+        # bottomLeftCornerOfText = (face_left_position+face_width+10, face_top_position + 20)
+        # cv2.putText(img, calm, bottomLeftCornerOfText, font_emotions, font_emotions_scale, font_emotions_color, lineType)
 
     if male+female == 0:
         cv2.imshow("window", img)
         cv2.moveWindow('window', 200, 200)
         cv2.waitKey(1)
         continue
-    female_percentage = (female / float(male+female)) * 100
+    female_percentage = math.ceil((female / float(male+female)) * 100)
     male_percentage = (male / float(male+female)) * 100
 
     font = cv2.FONT_HERSHEY_TRIPLEX
@@ -110,14 +114,15 @@ while True:
     lineType = 2
 
     cv2.putText(img, str(int(male_percentage)) + '% Male, ' + str(int(female_percentage))
-                + '% Female, % Avg calm: ' + str(int(total_calm/float(male+female)))
-                + ', % Avg surprised: ' + str(int(total_surprised/float(male+female)))
-                + ', % Avg confused: ' + str(int(total_confused / float(male + female))),
+                + '% Female, A. Happy ' + str(int(total_happy/float(male+female)))
+                + '%, A. Calm ' + str(int(total_calm/float(male+female)))
+                + '%, A. Confused ' + str(int(total_confused / float(male + female))) + '%',
                 bottomLeftCornerOfText,
                 font,
                 fontScale,
                 fontColor,
                 lineType)
+
     cv2.imshow("window", img)
     cv2.moveWindow('window', 200, 200)
     cv2.waitKey(1)
